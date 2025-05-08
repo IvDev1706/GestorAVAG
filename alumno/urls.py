fb359ff66ab3503d0761d328c73322f9bd1317ec
IvDev1706 <ivancdeno@gmail.com>
2025-05-08 15:08:09 -0600

    version estable 1

--- alumno/urls.py
+++ alumno/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     path("",login_view),
     path('logout/',logout_view),
     path('alumno/miinfo/',cliente_index),
-    path('alumno/history',cliente_history),
-    path('alumno/payment',cliente_payment),
+    path('alumno/history/',cliente_history),
+    path('alumno/payment/',cliente_payment),
     path('alumno/checkout/',checkout_session),
     path('stripe/webhook',stripe_webhook)
 ]
