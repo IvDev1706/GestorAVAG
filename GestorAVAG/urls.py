fc0974df354eb66589810719e64a33ca728fd487
IvDev1706 <ivancdeno@gmail.com>
2025-05-20 09:36:16 -0600

    version completa

--- GestorAVAG/urls.py
+++ GestorAVAG/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('admon/', include('gestor.urls')),
+    path('', include('alumno.urls'))
 ]
--- gestor/urls.py
+++ gestor/urls.py
@@ -10,15 +10,15 @@ urlpatterns = [
     path('alumno/', index),
     path('alumno/create', createAlumno),
     path('alumno/update', updateAlumno),
-    path('alumno/delet', deleteAlumno),
+    path('alumno/delete', deleteAlumno),
     path('pago/register', createPago),
     path('pago/history/<str:curp>/', historyPago),
     
     # Nuevas rutas con nombres
     path('alumno/update/buscar', buscar_alumno, name='buscar_alumno'),
     path('alumno/update/<str:curp>/', actualizar_alumno, name='actualizar_alumno'),
-    path('alumno/delet/buscar', buscar_alumno_eliminar, name='buscar_alumno_eliminar'),
-    path('alumno/delet/<str:curp>/', eliminar_alumno, name='eliminar_alumno'),
+    path('alumno/delete/buscar', buscar_alumno_eliminar, name='buscar_alumno_eliminar'),
+    path('alumno/delete/<str:curp>/', eliminar_alumno, name='eliminar_alumno'),
     path('pago/register/buscar', buscar_alumno_pago, name='buscar_alumno_pago'),
     path('pago/register/<str:curp>/', registrar_pago, name='registrar_pago'),
 ]
